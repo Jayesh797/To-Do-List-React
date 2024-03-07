de00abe80819e27a8800d4be9dcfce0e4fa6fc77
Jayesh797 <jayeshlandge797@gmail.com>
2024-03-07 18:51:59 +0530

    Main index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ToDolist from "./ToDolist";
+import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+ReactDOM.render(
+  <>
+    <ToDolist/>
+  </>,document.getElementById('root')
+);
