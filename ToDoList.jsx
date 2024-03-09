2b500e83f0d2635db9c20d88bf331d6e25b91f6b
Jayesh797 <jayeshlandge797@gmail.com>
2024-03-09 16:20:13 +0530

    updated ToDoList.jsx file

--- ToDoList.jsx
+++ ToDoList.jsx
@@ -2,42 +2,37 @@ import React, { useState } from "react";
 import { Button } from "@mui/material";
 import ListComponent from "./ListComponent";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-const ToDolist=()=>
-{
-    const [item,setItem]=useState()
-    const [newitem,setNewItem]=useState([])
-    const itemevent=(event)=>
-    {
+const ToDolist = () => {
+    const [item, setItem] = useState()
+    const [newitem, setNewItem] = useState([])
+    const itemevent = (event) => {
         setItem(event.target.value)
     }
-    const AddNew=()=>
-    {
-        setNewItem((prevVal)=>
-        {
-            return [...prevVal,item]
+    const AddNew = () => {
+        setNewItem((prevVal) => {
+            return [...prevVal, item]
         })
         setItem(' ')
     }
-    return(
+    return (
         <>
             <div className="main_div">
                 <div className="centre_div">
-                    <br/>
+                    <br />
                     <h1>TO DO LIST</h1>
-                    <br/>
-                    <input type="text" value={item} placeholder="Enter item" onChange={itemevent}/>
+                    <br />
+                    <input type="text" value={item} placeholder="Enter item" onChange={itemevent} />
                     <Button className="button" onClick={AddNew}>
-                        <AddCircleIcon/>
+                        <AddCircleIcon />
                     </Button>
-                    <ol>
-                        {
-                            newitem.map((val)=>
-                            {
-                                return <ListComponent text={val}/>
-                            })
-                        }
-                    </ol>
                 </div>
+                <ol>
+                    {
+                        newitem.map((val) => {
+                            return <ListComponent text={val} />
+                        })
+                    }
+                </ol>
             </div>
         </>
     )
