323be2972819f0a1785285b36e571cfa3b750621
Jayesh797 <jayeshlandge797@gmail.com>
2024-03-09 16:20:36 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ToDolist from "./ToDolist";
 import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 ReactDOM.render(
   <>
     <ToDolist/>
